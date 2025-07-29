7e68fa3c1402842110572a2e80bd75d320b2c69e
C'pharh <cpharh@MacBook-Pro-2.local>
2025-07-29 22:31:32 +0200

    tests fixed

--- src/Backend/Server.js
+++ src/Backend/Server.js
@@ -3,5 +3,5 @@ const app = require('../API/UserAPI');
 const dbconnection = require('../app/utils/dbconnection');
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
